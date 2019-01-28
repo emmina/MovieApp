f70dad96b60d00caabd6a967d8e2aea0b619404d
Emina <eminamuratovic14@gmail.com>
2019-01-28 02:22:58 +0100

    added Link from react router to navbar

--- src/helpers/Navbar.jsx
+++ src/helpers/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 class Navbar extends Component {
-
     onSearch(query) {
         this.props.onSearch(query.target.value)
     }
@@ -17,6 +16,8 @@ class Navbar extends Component {
                 <Link className={`nav-link ${active === 'movies' ? 'navlink-active' : ''}`} to='/movies'>Top 10 Movies</Link>
                 <Link className={`nav-link ${active === 'shows' ? 'navlink-active' : ''}`} to='/shows'>Top 10 TV Shows</Link>
                 <input className="form-inline form-control mr-sm-2 search-input" type="search" placeholder="Search" aria-label="Search" onChange={(e) => this.onSearch(e)} />
+
+
             </nav>
         );
     }
